ec27bc1fc2ee6efda0f428c92cd4d05b47a2f3ad
Tyrion1018 <joega068472@gmail.com>
2021-01-25 02:11:36 +0800

    Logistic Regression

--- main.py
+++ main.py
@@ -190,9 +190,9 @@ for dataset in combine:
 train_df = train_df.drop(['FareBand'], axis=1)
 combine = [train_df, test_df]
 
-print(train_df.head(10))
+# print(train_df.head(10))
 
-print(test_df.head(10))
+# print(test_df.head(10))
 
 X_train = train_df.drop("Survived", axis=1)
 Y_train = train_df["Survived"]
@@ -200,11 +200,89 @@ X_test = test_df.drop("PassengerId", axis=1).copy()
 X_train.shape, Y_train.shape, X_test.shape
 
 # Logistic Regression
-
 logreg = LogisticRegression()
 logreg.fit(X_train, Y_train)
 Y_pred = logreg.predict(X_test)
 acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
-print(acc_log)
+print("LOG score: " + '{:2.2f}'.format(acc_log))
 
-plt.show()
+# Print Correlation
+coeff_df = pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns = ['Feature']
+coeff_df["Correlation"] = pd.Series(logreg.coef_[0])
+
+# print(coeff_df.sort_values(by='Correlation', ascending=False))
+
+# Support Vector Machines
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+print("SVC score: " + '{:2.2f}'.format(acc_svc))
+
+# KNN or k-Nearest Neighbors
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+print("KNN score: " + '{:2.2f}'.format(acc_knn))
+
+# Gaussian Naive Bayes
+gaussian = GaussianNB()
+gaussian.fit(X_train, Y_train)
+Y_pred = gaussian.predict(X_test)
+acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+print("Gaussian score: " +  '{:2.2f}'.format(acc_gaussian))
+
+# Perceptron
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+print("Perceptron score: " + '{:2.2f}'.format(acc_perceptron))
+
+
+# Linear SVC
+linear_svc = LinearSVC()
+linear_svc.fit(X_train, Y_train)
+Y_pred = linear_svc.predict(X_test)
+acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
+print("Linear SVC score: " + '{:2.2f}'.format(acc_linear_svc))
+
+# Stochastic Gradient Descent
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+print("Stochastic Gradient Descent score: " + '{:2.2f}'.format(acc_sgd))
+
+# Decision Tree
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+print("Decision Tree score: " + '{:2.2f}'.format(acc_decision_tree))
+
+# Random Forest
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+random_forest.score(X_train, Y_train)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+print("Random Forest score: " + '{:2.2f}'.format(acc_random_forest))
+
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+              'Random Forest', 'Naive Bayes', 'Perceptron',
+              'Stochastic Gradient Decent', 'Linear SVC',
+              'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log,
+              acc_random_forest, acc_gaussian, acc_perceptron,
+              acc_sgd, acc_linear_svc, acc_decision_tree]})
+print( models.sort_values(by='Score', ascending=False) )
+
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred
+    })
+submission.to_csv('submission.csv', index=False)
